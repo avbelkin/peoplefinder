0c04fedc2a81d6259000e0d7d4f66d62b459b482
Alexey Belkin <48679054+avbelkin@users.noreply.github.com>
2021-02-18 20:34:49 +0300

    Update gpsd_client.py
    fix crashes when gps not available

--- com_interface/gpsd_client.py
+++ com_interface/gpsd_client.py
@@ -32,28 +32,30 @@ class GPSDListenerProcess(multiprocessing.Process):
                 self.logger.error("To establish connection with gpsd failed!")
                 time.sleep(0.1)
                 continue
+            try:
+                for report in self.__session:
+                    if self.__time_to_shutdown.is_set():
+                        break
 
-            for report in self.__session:
-                if self.__time_to_shutdown.is_set():
-                    break
+                    if report.get(u'class') == u'TPV':
+                        lat = report.get(u'lat')
+                        lon = report.get(u'lon')
 
-                if report.get(u'class') == u'TPV':
-                    lat = report.get(u'lat')
-                    lon = report.get(u'lon')
+                        time_str = report.get('time')
+                        if time_str is None:
+                            self.logger.error("GPS coordinate does not have timestamps: {0}".format(report))
+                            continue
 
-                    time_str = report.get('time')
-                    if time_str is None:
-                        self.logger.error("GPS coordinate does not have timestamps: {0}".format(report))
-                        continue
+                        time_timestamp = None
+                        try:
+                            time_timestamp = time.mktime(datetime.datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%S.%fZ").timetuple())
+                        except ValueError as err:
+                            self.logger.error("Cann't deformed time in gpsd message: {0}".format(err.message))
+                            continue
 
-                    time_timestamp = None
-                    try:
-                        time_timestamp = time.mktime(datetime.datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%S.%fZ").timetuple())
-                    except ValueError as err:
-                        self.logger.error("Cann't deformed time in gpsd message: {0}".format(err.message))
-                        continue
+                        self.__comms_model.add_gps_meas(time_timestamp, lat, lon)
 
-                    self.__comms_model.add_gps_meas(time_timestamp, lat, lon)
-
-            if not self.__time_to_shutdown.is_set():
-                self.logger.error("Connection with gpsd lost!")
+                if not self.__time_to_shutdown.is_set():
+                    self.logger.error("Connection with gpsd lost!")
+            except:
+                self.logger.error("failed to navigate self.__session")
