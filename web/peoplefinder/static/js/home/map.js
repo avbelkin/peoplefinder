b2a15bac174278c7d2397cfe107430f0979a811f
Alexey Belkin <48679054+avbelkin@users.noreply.github.com>
2021-02-18 20:59:31 +0300

    change initial map position

--- web/peoplefinder/static/js/home/map.js
+++ web/peoplefinder/static/js/home/map.js
@@ -31,7 +31,7 @@
 
 
         setInitialView: function () {
-            this._map.setView([-26.017, 139.219], 4);
+            this._map.setView([60, 30], 6);
         }
     });
 }(jQuery, pf, L));
